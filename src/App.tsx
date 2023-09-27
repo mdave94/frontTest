5c86652abd8e65b8d634dee7396e49ebe271a0d8
mdave94 <molnardavid2002@gmail.com>
2023-09-27 15:42:46 +0200

    Footer Component is added

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import twoItem from "./assets/img/image07.jpg";
 import dividerPict from "./assets/img/image02.jpg";
 import InfoSection from "./components/InfoSection";
 import SwiperComponent from "./components/SwiperComponent";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -26,31 +27,8 @@ function App() {
       <div className="center-container">
         <SwiperComponent title="Galéria" />
       </div>
-      <ParallaxComponent imageUrl={twoItem}>
-        Tisztán tartás: Nyissa ki a tisztító folyadékot tartalmazó üvegcsét! Az
-        üvegcsét "Tisztító folyadék" felirattal láttuk el! Vegye ki a tisztító
-        tégelyt a csomagolásból! Nyissa ki a tégelyt, majd alaposan öblítse ki
-        tiszta vízzel, végül törölje szárazra! Az ékszer hatékony tisztítása
-        érdekében adagoljon annyi tisztító folyadékot a tégelybe, hogy az
-        ékszerét éppen ellepje! A kitöltött folyadék alkalmas többszöri
-        tisztításra (maximum 5 alkalom/egy nap), de minden tisztítás előtt
-        pótolni kell az elpárolgott folyadék mennyiséget. Mindig gondoskodjon
-        arról, hogy a tisztító tégely teteje pontosan és szorosan záródjon!
-        Amennyiben látható szennyeződés van az ékszeren, azt le kell róla
-        törölni, majd helyezze a folyadékkal töltött tégelybe az ékszert. Hagyja
-        ázni 2 percig, majd vegye ki a tégelyből! A folyadék pár másodperc alatt
-        elpárolog róla. Az ékszer most készen áll a behelyezésre! Amennyiben
-        jelenleg nem szeretné viselni az ékszert, helyezze szárazon a tároló
-        tégelybe. Az ékszer viselése előtt és után mindig alaposan mosson fogat!
-        A tökéletes szájhigiénia eléréséhez ajánlott fogselymet és
-        fogköztisztító kefét is használni! Kérje fogorvosa vagy
-        dentálhigiénikusa tanácsát az Ön számára legjobban megfelelő, egyedi
-        szájhigiéniai rutin kialakításához! Évente látogasson el fogászati
-        ellenőrzésre, mert minden fogászati probléma legjobb megoldása a
-        megelőzés!
-      </ParallaxComponent>
-
-      <div className="content-red"></div>
+      <ParallaxComponent imageUrl={twoItem}></ParallaxComponent>
+      <Footer />
     </div>
   );
 }
