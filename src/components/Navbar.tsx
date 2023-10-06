d3da258429b4a845a707b59f8d17862238e798d9
mdave94 <molnardavid2002@gmail.com>
2023-10-06 14:30:15 +0200

    navbar mobile view set

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,17 @@ import { useEffect, useState } from "react";
 import "../assets/css/navbar.css";
 import logo from "../assets/png/image01.png";
 import { Link, useNavigate } from "react-router-dom";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const Navbar = () => {
   const [isVisible, setIsVisible] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
   const navigate = useNavigate();
 
   useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
     const handleScroll = () => {
       if (window.scrollY > 50) {
         setIsVisible(true);
@@ -18,27 +23,48 @@ const Navbar = () => {
 
     window.addEventListener("scroll", handleScroll);
 
+    window.addEventListener("resize", handleResize);
+
     return () => {
       window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
-  return (
-    <nav className={`navbar ${isVisible ? "visible" : "hidden"}`}>
-      <img id="nav-logo" src={logo} alt=" " onClick={() => navigate("/")} />
-      <div className="nav-menu-list">
-        <Link to="/history">
-          <a>TÖrténetünk</a>
-        </Link>
-        <Link to="/informations">
-          <a>Információk</a>
-        </Link>
-        <Link to="/userinstructions">
-          <a>Tájékoztatók</a>
-        </Link>
-      </div>
-    </nav>
-  );
+  if (isMobile) {
+    return (
+      <>
+        <div className="mobile-navbar-container">
+          <div className="mobile-nav-left">
+            <div className="mobile-logo">M</div>
+          </div>
+          <div className="mobile-nav-right">
+            <div className="appointment">IDŐPONT</div>
+            <div className="burger">
+              <RxHamburgerMenu />
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <nav className={`navbar ${isVisible ? "visible" : "hidden"}`}>
+        <img id="nav-logo" src={logo} alt=" " onClick={() => navigate("/")} />
+        <div className="nav-menu-list">
+          <Link to="/history">
+            <a>TÖrténetünk</a>
+          </Link>
+          <Link to="/informations">
+            <a>Információk</a>
+          </Link>
+          <Link to="/userinstructions">
+            <a>Tájékoztatók</a>
+          </Link>
+        </div>
+      </nav>
+    );
+  }
 };
 
 export default Navbar;
