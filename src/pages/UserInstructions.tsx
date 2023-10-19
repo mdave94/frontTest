c72a3766aa736197dbcfc3b7a138f62a8b75c03d
mdave94 <molnardavid2002@gmail.com>
2023-10-19 12:10:33 +0200

    userinstructuions page is added

--- src/pages/UserInstructions.tsx
+++ src/pages/UserInstructions.tsx
@@ -1,4 +1,9 @@
 import { useEffect } from "react";
+import ParallaxComponent from "../components/ParallaxComponent";
+import mirrorPicture from "../assets/img/smile1.jpg";
+import divider1 from "../assets/img/smile2.jpg";
+import divider2 from "../assets/img/model2.jpg";
+import divider3 from "../assets/img/model1.jpg";
 
 function UserInstructions() {
   useEffect(() => {
@@ -6,7 +11,62 @@ function UserInstructions() {
     window.scrollTo(0, 0);
   }, []);
 
-  return <>userInstrucions</>;
+  return (
+    <>
+      <>
+        <div className="image-container">
+          <img src={mirrorPicture} />
+        </div>
+        <div className=" center-container">
+          <div className="header center">Használati útmutató:</div>
+          <div className="section-b">
+            Az első lépés az ékszer csomagolásának eltávolítása! Nyissa ki a
+            tisztító folyadékot tartalmazó üvegcsét! Az üvegcsét "Tisztító
+            folyadék" felirattal láttuk el! Vegye ki a tisztító tégelyt a
+            csomagolásból! Nyissa ki a tégelyt, majd alaposan öblítse ki tiszta
+            vízzel, végül törölje szárazra! Az ékszer hatékony tisztítása
+            érdekében adagoljon annyi tisztító folyadékot a tégelybe, hogy az
+            ékszerét éppen ellepje! A kitöltött folyadék alkalmas többszöri
+            tisztításra (maximum 5 alkalom), de minden tisztítás előtt pótolni
+            kell az elpárolgott folyadék mennyiséget. Mindig gondoskodjon arról,
+            hogy a tisztító tégely teteje pontosan és szorosan záródjon! Hagyja
+            ázni az ékszert 2 percig, majd vegye ki a tégelyből! A folyadék pár
+            másodperc alatt elpárolog róla. Az ékszer most készen áll a
+            behelyezésre!
+            <br></br>
+            <br></br>
+            Az ékszert helyezze be a megfelelő fogközbe ujjai vagy egy csipesz
+            segítségével. Az ékszer stiftjét óvatosan illessze a fog és az íny
+            találkozásához, majd finom mozdulatokkal vezesse át a fogak között.
+            Ha vérzést tapasztal, keresse fel fogorvosát vagy
+            dentálhigiénikusát. Az ékszer behelyezése / viselése közben
+            jelentkező ínyvérzés az ínygyulladás tünete lehet, mely fogászati
+            ellátást igényel. Behelyezés után az ékszer nyugodtan viselhető,
+            amíg kényelmesnek érződik. Ugyanakkor javasolt az ékszert legfeljebb
+            4 óra viselés után kivenni és az ínyt ??? órán keresztül pihenni
+            hagyni.
+          </div>
+        </div>
+
+        <ParallaxComponent imageUrl={divider1} minHeight="200px" />
+        <div className=" center-container">
+          <div className="section-b">
+            <div className="header center">Figyelmeztetés </div>
+            <div className="section-b">
+              Fogágybetegség esetén a mosoly ékszer viselése nem javasolt, mert
+              fokozhatja a parodontológiai tüneteket, valamint gyorsíthatja a
+              betegség progresszióját Ha fájdalmat tapasztal, távolítsa el a
+              mosoly ékszert a fogai közül Mosolyékszer 16 éves kor alatt nem
+              viselhető (18 áven aluli és gondozás alatt álló személyek esetén a
+              gondviselő beleegyezésére van szükség.)
+            </div>
+            <div className="custom-button">Időpontfoglalás</div>
+          </div>
+        </div>
+        <ParallaxComponent imageUrl={divider3} minHeight="200px" />
+      </>
+    </>
+  );
 }
 
 export default UserInstructions;
