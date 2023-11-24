ee6b3b9a72fa1a2c1cc5e4ee9d47916df042636d
molnar.david <molnar.david@wigner.hu>
2023-11-24 10:19:39 +0100

    history page margin

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -23,13 +23,13 @@ function History() {
       <div className="container center">
         <div className="section-b">
           <div className="section-header">történetünk</div>
-          <p>
+          <div className="section-b">
             A Minnae-nél hisszük, hogy a legszebb ékszer egy magabiztos mosoly.
             Ezt szeretnénk kiemelni minden termékünkkel. Azoknak tervezünk, akik
             szeretnék a megjelenésüket egy igazán egyedi kiegészítővel
             felejthetetlenné tenni, hiszen mosolyékszereink páratlan
             eleganciával ruházzák fel viselőjüket.
-          </p>
+          </div>
         </div>
       </div>
 
@@ -37,32 +37,36 @@ function History() {
 
       <div className="container center">
         <div className="section-b">
-          <div className="section-header">a MINNAE története</div>
-          <p>
-            Egy mosoly lehet a kezdete életünk legizgalmasabb történeteinek, jó
-            érzés adni és kapni is.
-          </p>
-          <p>
-            Mi azt tűztük ki célként, hogy ehhez tervezzünk egy ékszert, ami
-            segít a mosolyokra vonni a figyelmet.
-          </p>
+          <div className="section-header">a minnae története</div>
+          <div className="section-b">
+            <p>
+              Egy mosoly lehet a kezdete életünk legizgalmasabb történeteinek,
+              jó érzés adni és kapni is.
+            </p>
+            <p>
+              Mi azt tűztük ki célként, hogy ehhez tervezzünk egy ékszert, ami
+              segít a mosolyokra vonni a figyelmet.
+            </p>
+          </div>
         </div>
       </div>
       <ParallaxComponent imageUrl={divider1} minHeight="200px" />
       <div className=" full-center flex-column ">
         <div className="section-b">
           <div className="section-header"> honnan jött az ötlet? </div>
-          “Kiskoromban édesanyám egyik munkatársnőjén láttam először fogékszert.
-          Azonnal megtetszett és én is nagyon szerettem volna egy apró díszt a
-          fogaimra. Aztán anyukám fogorvos lévén elmagyarázta, hogy milyen
-          hátrányai lehetnek egy ilyen ékszernek. Mindig is nagyon büszke voltam
-          arra, hogy a fogaim teljesen egészségesek, így ezt semmiképp sem
-          akartam kockáztatni. Később, mikor már én is fogorvosként dolgoztam,
-          továbbra is foglalkoztatott a gondolat, hogy a hagyományos fogékszerek
-          egyikét sem tudom őszintén ajánlani a pácienseimnek. Ekkor született
-          meg az ötlet, hogy tervezek egy saját ékszert, amit én is szívesen
-          viselnék és bárkinek jószívvel ajánlhatok. A minnae mosolyékszerekkel
-          ezt sikerült elérnem.”
+          <div className="section-b">
+            “Kiskoromban édesanyám egyik munkatársnőjén láttam először
+            fogékszert. Azonnal megtetszett és én is nagyon szerettem volna egy
+            apró díszt a fogaimra. Aztán anyukám fogorvos lévén elmagyarázta,
+            hogy milyen hátrányai lehetnek egy ilyen ékszernek. Mindig is nagyon
+            büszke voltam arra, hogy a fogaim teljesen egészségesek, így ezt
+            semmiképp sem akartam kockáztatni. Később, mikor már én is
+            fogorvosként dolgoztam, továbbra is foglalkoztatott a gondolat, hogy
+            a hagyományos fogékszerek egyikét sem tudom őszintén ajánlani a
+            pácienseimnek. Ekkor született meg az ötlet, hogy tervezek egy saját
+            ékszert, amit én is szívesen viselnék és bárkinek jószívvel
+            ajánlhatok. A minnae mosolyékszerekkel ezt sikerült elérnem.”
+          </div>
         </div>
         <div id="prof-section-wrapper">
           <div className="prof-section">
@@ -78,26 +82,28 @@ function History() {
       <div className="container center ">
         <div className="section-b">
           <div className="section-header">célunk</div>
-          <p>
-            A MINNAE mosolyékszerek úgy lettek tervezve, hogy kihangsúlyozzák az
-            egészséges fogak és fogíny szépségét.
-          </p>
-          <p>
-            Ugyanakkor felhívják a figyelmet az ínygyulladásra is, amely a
-            felnőtt lakosság közel 70%-át érinti.
-          </p>
-          <p>
-            A fogínygyulladás kezeletlenül fogágybetegséghez vezethet, ami a
-            fogak elvesztésének egyik leggyakoribb oka.
-          </p>
-          <p>
-            Célunk a nagyfokú szerepvállalas a fogászati prevencióban, mely
-            lehetővé teszi a fogak élethosszig tartó megőrzését.
-          </p>
-          <p>
-            Folyamatos motivációnk, hogy a MINNAE mosolyékszerek, ne csak a
-            szép, hanem az egészséges száj hírnökei legyenek.
-          </p>
+          <div className="section-b">
+            <p>
+              A MINNAE mosolyékszerek úgy lettek tervezve, hogy kihangsúlyozzák
+              az egészséges fogak és fogíny szépségét.
+            </p>
+            <p>
+              Ugyanakkor felhívják a figyelmet az ínygyulladásra is, amely a
+              felnőtt lakosság közel 70%-át érinti.
+            </p>
+            <p>
+              A fogínygyulladás kezeletlenül fogágybetegséghez vezethet, ami a
+              fogak elvesztésének egyik leggyakoribb oka.
+            </p>
+            <p>
+              Célunk a nagyfokú szerepvállalas a fogászati prevencióban, mely
+              lehetővé teszi a fogak élethosszig tartó megőrzését.
+            </p>
+            <p>
+              Folyamatos motivációnk, hogy a MINNAE mosolyékszerek, ne csak a
+              szép, hanem az egészséges száj hírnökei legyenek.
+            </p>
+          </div>
         </div>
       </div>
     </>
