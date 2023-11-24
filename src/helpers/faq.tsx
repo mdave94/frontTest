6812582b86041b8087c1c1aa49a60e1ade8e079c
molnar.david <molnar.david@wigner.hu>
2023-11-24 10:45:25 +0100

    faq update

--- src/helpers/faq.tsx
+++ src/helpers/faq.tsx
@@ -17,50 +17,39 @@ export const faqArray = [
     answer:
       "Mosolyékszereinkre 3 év garanciát vállalunk. Az ékszer sérülése/törése esetén töltsd ki weboldalunkon elérhető hiba bejelentő űrlapunkat. Ezután végig vezetünk a szükséges lépéseken, hogy újra élvezhesd a mosolyékszerviselés élményét.",
   },
+
   {
     id: 3,
-    question: "Lenyelhetem az ékszert?",
-    answer:
-      "Mosolyékszereinkre 3 év garanciát vállalunk. Az ékszer sérülése/törése esetén töltsd ki weboldalunkon elérhető hiba bejelentő űrlapunkat. Ezután végig vezetünk a szükséges lépéseken, hogy újra élvezhesd a mosolyékszerviselés élményét.",
-  },
-  {
-    id: 4,
-    question: "Mi történik, ha az ékszer megsérül, eltörik?",
-    answer:
-      "Mosolyékszereinkre 3 év garanciát vállalunk. Az ékszer sérülése/törése esetén töltsd ki weboldalunkon elérhető hiba bejelentő űrlapunkat. Ezután végig vezetünk a szükséges lépéseken, hogy újra élvezhesd a mosolyékszerviselés élményét.",
-  },
-  {
-    id: 4,
     question: "Előfordulhat, hogy lenyelem az ékszert?",
     answer:
       "Úgy terveztük, hogy az ékszer stabilan álljon a helyén. Mozgását a nem megfelelő méret, vagy rossz szokásként az ékszer piszkálása, mozgatása okozhatja. Célszerű tehát figyelni, hogy az ékszer stabilan áll-e és törekedni a stabil pozícióban tartására.  Ha mégis kimozdul a helyéről, az általában azonnal feltűnik a viselőjének. Ha az ékszert étkezés közben viseljük, az növelheti a kimozdulás kockázatát és egy falattal együtt akár le is nyelhetjük. Ezért nem javasolt az ékszer viselése étkezés közben. Ha mégis bekövetkezne, az alábbiakat érdemes tudni. Az ékszer anyaga nem fejt ki káros hatást a szervezetre. Kövesd figyelemmel, hogy természetes úton távozik-e. Amennyiben bármilyen emésztőrendszeri tünetet, fájdalmat tapasztalsz, esetleg változást az anyagcserédben, haladéktalanul fordulj orvoshoz!",
   },
   {
-    id: 5,
+    id: 4,
     question: "Probléma, ha korona/héjpótlás van a fogamon?",
     answer:
       "Az ékszer megfelelően készített, ép korona/héj mellett is használható. Nem megfelelő használat, vagy a korona/héj nem tökéletes állapota esetén viszont a koronán/héjon is kopást okozhat. Koronával/héjjal borított fog mellett fokozott óvatosság szükséges az alkalmazáshoz, és javasolt a rendszeres fogászati ellenőrzés!",
   },
   {
-    id: 6,
+    id: 5,
     question: "Probléma, ha hídpótlás van a fogaimon?",
     answer:
       " Hídpótlással általában foghiányt pótolnak. Mivel a hiányzó fog és a hiánnyal szomszédos fog között nincs kialakítva átjárható fogköz, sajnos ide nem lehet behelyezni mosolyékszert. Másik fogközben viszont valószínűleg gond nélkül viselhető. Az ékszer kiválasztásakor segítünk felmérni, hogy melyik fogközök alkalmasak a kiválasztott ékszer viselésére.",
   },
   {
-    id: 7,
+    id: 6,
     question: "Probléma, ha inlay/onlay/overlay pótlás van a fogamon?",
     answer:
       "Az ékszer megfelelően készített, ép inlay/onlay/overlay pótlás mellett is használható. Nem megfelelő használat, vagy a pótlás nem tökéletes állapota esetén viszont a pótláson is kopást okozhat. Inlay/onlay/overlay pótlással ellátott fog mellett fokozott óvatosság szükséges az alkalmazáshoz, és javasolt a rendszeres fogászati ellenőrzés!",
   },
   {
-    id: 8,
+    id: 7,
     question: "Réses fogazat (diasthema) esetén is viselhető mosolyékszer?",
     answer:
       "A fogak közötti rés mérete határozza meg, hogy viselhető-e mosolyékszer. Ezt megállapíthatjuk és  alaposan átbeszélhetjük az első mosolyékszer megtervezésekor.",
   },
   {
-    id: 9,
+    id: 8,
     question:
       "Fémet tartalmazó fogtömés/fogpótlás található a számban (amalgám, arany, titán) mi a teendő?",
     answer:
